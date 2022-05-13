24cea7383313a09c9a904ad5baa3281839760aad
Joe Williams <joe.md.williams@gmail.com>
2022-05-13 23:47:01 +0100

    Create Project & Group Structure

---
+++ GripXPokemonApp/GripXPokemonApp/Components/UI/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GripXPokemonApp
+//
+//  Created by Joe on 13/05/2022.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
---
+++ GripXPokemonApp/GripXPokemonApp/GripXPokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GripXPokemonAppApp.swift
+//  GripXPokemonApp
+//
+//  Created by Joe on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GripXPokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
